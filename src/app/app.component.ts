dff141936669bdc8cfc28692e5a6e09623da3afe
sreekalaks <sreekalaks808@gmail.com>
2022-12-26 20:00:02 +0530

    Todo list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Todo';
+  title = 'Todo list';
+  todos = [
+    {
+      label:'Bring milk',
+      done: false,
+      priority:3
+    },
+    {
+      label:'Pay mobile bill',
+      done:true,
+      priority:1
+    },
+    {
+      label:'clean house',
+      done: false,
+      priority:4
+    },
+    {
+      label:'Fix the bulb',
+      done: false,
+      priority:5
+    },
+  ];
+
+  addTodo(newTodoLabel: any){
+   var newTode = {
+      label: newTodoLabel,
+      priority:1,
+      done: false,
+   };
+   this.todos.push(newTode);
+  }
+
+  deleteTodo(todo:any) {
+    this.todos = this.todos.filter( t => t.label !== todo.label);
+  }
 }
+
+    
